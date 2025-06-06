d1b3f7ab35574a91975a025e7285548546e47794
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-06-06 11:15:14 +0700

    inisialisasi status ambil burung dari bloc

--- lib/presentation/buyer/home/buyer_home_screen.dart
+++ lib/presentation/buyer/home/buyer_home_screen.dart
@@ -9,6 +9,12 @@ class BuyerHomeScreen extends StatefulWidget {
 
 class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
   @override
+  void initState() {
+    super.initState();
+    context.read<GetBurungTersediaBloc>().add(GetAllBurungTersediaEvent());
+  }
+  
+  @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
