cf17a45566351ca6bb6626496ac8296fc155c0e9
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-06-06 14:45:55 +0700

    BLoC events untuk registrasi

---
+++ lib/presentation/auth/bloc/register/bloc/register_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:canary_app/data/model/request/auth/register_request_model.dart';
+import 'package:meta/meta.dart';
+
+part 'register_event.dart';
+part 'register_state.dart';
+
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  RegisterBloc() : super(RegisterInitial()) {
+    on<RegisterEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
